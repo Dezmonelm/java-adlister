005c8d4ab98009591ccbe780b6784d4df9e5357f
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2021-12-06 09:23:00 -0500

    cookies

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        // inside of a servlet
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("logged-in") != null) {
+            request.setAttribute("logged", true);
+        } else {
+            response.sendRedirect("/login");
+        }
+
     }
 }
